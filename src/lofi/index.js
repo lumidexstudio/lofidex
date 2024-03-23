97551ddc092c49fdc9ad957211c0d09cf471bff5
JastinXyz <jastinlinggar@gmail.com>
2024-03-23 17:37:24 +0700

    feat: first lofi for testing purpose

---
+++ src/lofi/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = [
+    {
+        id: 1,
+        title: "Chill Lofi Hiphop - ‘Kimochi’",
+        author: 'FrKA2',
+        source: 'https://soundcloud.com/nocopyrightlofi/lofaki',
+        path: path.join(__dirname, '1.mp3')
+    }
+]
