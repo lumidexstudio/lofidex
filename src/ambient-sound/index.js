d3f56ce1bea16f1c8ac69dd771785a610b6590bf
JastinXyz <jastinlinggar@gmail.com>
2024-03-24 14:07:55 +0700

    rain looop mp3

--- src/ambient-sound/index.js
+++ src/ambient-sound/index.js
@@ -5,7 +5,7 @@ module.exports = {
     {
       title: "rainy day in town",
       source: "https://pixabay.com/sound-effects/rainy-day-in-town-with-birds-singing-194011/",
-      path: path.join(__dirname, "rain10s.mp3"),
+      path: path.join(__dirname, "rains.mp3"),
     },
   ],
 };
