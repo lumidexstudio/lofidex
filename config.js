2c5dbccc348cf23b67b96500aa33574b9ea98f5a
JastinXyz <jastinlinggar@gmail.com>
2024-03-24 11:44:24 +0700

    add another prefix

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
   token: process.env.TOKEN,
   clientID: process.env.CLIENT_ID,
   ownerID: process.env.OWNER_ID.split(", "),
-  prefix: ["lumi", "lumi "],
+  prefix: ["lumi", "lumi ", "ldx", "ldx "],
 };
