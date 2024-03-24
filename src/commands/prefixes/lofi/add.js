3e21d5e1b5525938344b7a7bf837d9cb3bb3d3b6
JastinXyz <jastinlinggar@gmail.com>
2024-03-24 10:48:08 +0700

    ambient bisa

--- src/commands/prefixes/lofi/add.js
+++ src/commands/prefixes/lofi/add.js
@@ -1,14 +1,35 @@
 const { VoiceConnectionStatus, enterState, createAudioPlayer, createAudioResource, AudioPlayerStatus, getVoiceConnection } = require("@discordjs/voice");
 const ambientList = require("../../../ambient-sound");
+const { mixAudio } = require('ffmpeg-audio-mixer');
+const { StreamType } = require("@discordjs/voice");
+const fs = require('fs')
 
-const addAmbient = async (message, player, argsAmbient) => {
-  let ambient = ambientList[argsAmbient][0];
-
-  const res = createAudioResource(ambient.path, {
-    metadata: { title: ambient.title, author: ambient.author, source: ambient.source },
+async function simpanStreamKeFile(stream, path) {
+  return new Promise((resolve, reject) => {
+      const writeStream = fs.createWriteStream(path);
+      stream.pipe(writeStream);
+      writeStream.on('finish', resolve);
+      writeStream.on('error', reject);
   });
+}
 
-  player.play(res);
+const addAmbient = async (message, con, argsAmbient) => {
+  let ambient = ambientList[argsAmbient][0];
+  let list = require("../../../lofi");
+
+  // belum di cek posisi mana lagu berjalan
+  let checkNow = await message.client.db.has(`vc.${message.guild.id}.now`);
+  let song = list[0]
+
+  let d = await mixAudio([
+    song.path,
+    ambient.path
+  ]).toStream('mp3')
+  const outputPath = './tersimpan.mp3'; 
+  await simpanStreamKeFile(d, outputPath);
+
+  const res = createAudioResource(outputPath, { inputType: StreamType.Raw });
+  con.state.subscription.player.play(res);
 };
 
 module.exports = {
@@ -27,21 +48,24 @@ module.exports = {
     if(!ambientList[args[0]]) return message.reply("ambient not found")
 
     const connection = getVoiceConnection(message.guild.id);
+    addAmbient(message, connection, args[0])
+    // const player = new PlaySong().player
+    // player.pause();
 
-    const player = createAudioPlayer();
-    connection.subscribe(player);
-    addAmbient(message, player, args[0]);
+    // const player = createAudioPlayer();
+    // connection.subscribe(player);
+    // addAmbient(message, player, args[0]);
 
-    player.on(AudioPlayerStatus.Buffering, () => {
-      message.reply("buffering...");
-    });
+    // player.on(AudioPlayerStatus.Buffering, () => {
+    //   message.reply("buffering...");
+    // });
 
-    player.on(AudioPlayerStatus.Playing, () => {
-      message.reply("adding ambient");
-    });
+    // player.on(AudioPlayerStatus.Playing, () => {
+    //   message.reply("adding ambient");
+    // });
 
-    player.on("error", (error) => {
-      console.log(error);
-    });
+    // player.on("error", (error) => {
+    //   console.log(error);
+    // });
   },
 };
