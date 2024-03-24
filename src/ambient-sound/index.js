d45bd12d08bd993bbfebdd0dfbaa365d0866f841
JastinXyz <jastinlinggar@gmail.com>
2024-03-24 21:08:29 +0700

    optimize add ambient

--- src/ambient-sound/index.js
+++ src/ambient-sound/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   rains: [
     {
+      name: 'rains',
       title: "Short Rain Loop",
       source: "https://pixabay.com/sound-effects/short-rain-loop-101550/",
       path: path.join(__dirname, "rains.mp3"),
@@ -10,6 +11,7 @@ module.exports = {
   ],
   campfire: [
     {
+      name: 'campfire',
       title: "Burning Fire Loop 1",
       source: "https://pixabay.com/sound-effects/burning-fire-loop-1-188211/",
       path: path.join(__dirname, "campfire.mp3"),
