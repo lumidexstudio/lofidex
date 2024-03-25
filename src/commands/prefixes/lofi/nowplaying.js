0b4fe3ab6f80357a64050889e1c6355f530ad3dd
JastinXyz <jastinlinggar@gmail.com>
2024-03-25 14:15:29 +0700

    now playing buttons

--- src/commands/prefixes/lofi/nowplaying.js
+++ src/commands/prefixes/lofi/nowplaying.js
@@ -5,6 +5,9 @@ const formatTime = require('../../../lib/formatTime');
 const getCurrentlyPlayingTime = require('../../../lib/getCurrentPlayingTime');
 const createProgressBar = require('../../../lib/createProgressBar');
 const { default: getAudioDurationInSeconds } = require('get-audio-duration');
+const { ButtonBuilder, ButtonStyle } = require('discord.js');
+const { ActionRowBuilder } = require('discord.js');
+const { AudioPlayerStatus } = require('@discordjs/voice');
 
 module.exports = {
   name: "nowplaying",
@@ -15,6 +18,7 @@ module.exports = {
     let isplaying = await message.client.db.has(`vc.${message.guild.id}.now`);
     if(!isplaying) return message.reply("does'nt play any song rn");
 
+    let host = await message.client.db.get(`vc.${message.guild.id}.master`);
     let now = await message.client.db.get(`vc.${message.guild.id}.now`);
     let detail = songlist[now];
 
@@ -31,6 +35,68 @@ module.exports = {
         .setTimestamp()
         .setFooter({ text: `Song ID: ${detail.id}` });
 
-    message.reply({ embeds: [embed] });
+    let btns = {
+      pause: new ButtonBuilder().setCustomId('pause').setLabel('Pause').setEmoji("⏸").setStyle(ButtonStyle.Secondary),
+      stop: new ButtonBuilder().setCustomId('stop').setLabel('Stop').setEmoji("⏹").setStyle(ButtonStyle.Danger)
+    }
+
+    // buggy btns.pause
+    let row = new ActionRowBuilder().addComponents(btns.stop);
+    let msg = await message.channel.send({ embeds: [embed], components: [row] });
+
+    const collector = message.channel.createMessageComponentCollector({ time: 120000 });
+    collector.on("collect", async (d) => {
+      const set = async (x) => {
+        if(x.customId === 'pause') {
+          let player = connection.state.subscription.player;
+          if(player.state.status === AudioPlayerStatus.Paused) {
+              player.unpause();
+              btns.pause.setStyle(ButtonStyle.Secondary).setLabel('Pause').setEmoji("⏸");
+              msg.edit({ embeds: [embed], components: [row] })
+              return;
+          } else {
+              player.pause();
+              btns.pause.setStyle(ButtonStyle.Primary).setLabel('Resume').setEmoji("▶");
+          }
+        } else if(x.customId === 'stop') {
+          collector.stop('disconnect');
+        }
+
+        msg.edit({ embeds: [embed], components: [row] })
+        const collector2 = message.channel.createMessageComponentCollector({ time: 120000 });
+        collector2.on("collect", async (i) => {
+          set(i);
+        });
+      };
+
+      await d.deferUpdate();
+      if(d.user.id !== host) {
+        return d.followUp({
+          content: `${d.user.username}, only host can use this button.`,
+          ephemeral: true,
+        });
+      }
+
+      set(d);
+    });
+
+    collector.on('end', async(d, r) => {
+      if(r === 'disconnect') {
+        try {
+          connection.disconnect();
+          await msg.delete();
+          await message.client.db.delete(`vc.${message.guild.id}`);
+
+          let embed = new EmbedBuilder().setTitle("Disconnected").setTimestamp().setColor("Random");
+          message.replyWithoutMention({ embeds: [embed] });
+        } catch {
+          console.log("err stop button now playing")
+        }
+
+        return;
+      }
+
+      return;
+    })
   },
 };
