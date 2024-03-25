9fedac00304d9322c6f3abf7fe4580bccc5b0da1
JastinXyz <jastinlinggar@gmail.com>
2024-03-25 14:37:20 +0700

    Add cover too

--- src/commands/prefixes/lofi/add.js
+++ src/commands/prefixes/lofi/add.js
@@ -82,7 +82,7 @@ const addAmbient = async (message, con, argsAmbient) => {
           const res = createAudioResource(path, {
             inputType: StreamType.Raw,
             inlineVolume: true,
-            metadata: { title: song.title, author: song.author, source: song.source, path: song.path, index: list.findIndex((item) => item.title == song.title) },
+            metadata: { title: song.title, author: song.author, source: song.source, path: song.path, cover: song.cover, index: list.findIndex((item) => item.title == song.title) },
           });
 
           con.state.subscription.player.play(res);
