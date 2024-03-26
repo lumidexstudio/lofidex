6821dc2d354ac39cfb1704f5e751596dc675dc9f
Mayopi <cyade007@gmail.com>
2024-03-26 18:34:50 +0700

    fix: error EBUSY: resource busy or locked, unlink (?)

--- src/commands/prefixes/lofi/stop.js
+++ src/commands/prefixes/lofi/stop.js
@@ -1,7 +1,7 @@
 const { getVoiceConnection } = require("@discordjs/voice");
 const { EmbedBuilder } = require("discord.js");
 const { errorEmbed, successEmbed } = require("../../../lib/embed");
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports = {
   name: "stop",
@@ -10,18 +10,18 @@ module.exports = {
   category: "lofi",
   async execute(message) {
     let isplaying = await message.client.db.has(`vc.${message.guild.id}.now`);
-    if(!isplaying) return message.replyWithoutMention({ embeds: [errorEmbed('The bot is not playing music right now.')] });
+    if (!isplaying) return message.replyWithoutMention({ embeds: [errorEmbed("The bot is not playing music right now.")] });
 
     let getdb = await message.client.db.get(`vc.${message.guild.id}`);
-    if (getdb.master !== message.member.user.id) return message.replyWithoutMention({ embeds: [errorEmbed('Only the DJ can control using this command.')] });
+    if (getdb.master !== message.member.user.id) return message.replyWithoutMention({ embeds: [errorEmbed("Only the DJ can control using this command.")] });
     if (getdb.channel !== message.member.voice.channelId) return message.replyWithoutMention({ embeds: [errorEmbed(`We are not in the same voice channel!`)] });
 
     let connection = getVoiceConnection(message.guild.id);
-    if(!connection) return message.replyWithoutMention({ embeds: [errorEmbed('The bot is not playing music right now.')] });
+    if (!connection) return message.replyWithoutMention({ embeds: [errorEmbed("The bot is not playing music right now.")] });
 
-    await connection.disconnect();
-    await fs.rmSync(`temp/${message.guild.id}`, { recursive: true, force: true });
+    connection.disconnect();
+    fs.rmSync(`temp/${message.guild.id}`, { recursive: true, force: true });
     await message.client.db.delete(`vc.${message.guild.id}`);
-    message.replyWithoutMention({ embeds: [successEmbed('Disconnected')] });
+    message.replyWithoutMention({ embeds: [successEmbed("Disconnected")] });
   },
 };
