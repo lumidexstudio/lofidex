f9eda7ca88678391ec2f5089cb50f2f5a40f1a74
Mayopi <cyade007@gmail.com>
2024-04-06 12:30:02 +0700

    feat: lofi 5, 6, 7

--- src/lofi/index.js
+++ src/lofi/index.js
@@ -1,28 +1,60 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = [
-    {
-        id: 1, // tidak terlalu digunakan mungkin? gunakan index ajah
-        title: "Lazy Day",
-        author: "Lofi Velvet",
-        source: "https://soundcloud.com/lofivelvet/lazy-day",
-        cover: "https://i1.sndcdn.com/artworks-0Hu58R3AY8YiIJ14-ixjYbA-t500x500.jpg",
-        path: path.join(__dirname, '1.mp3')
-    },
-    { 
-        id: 2,
-        title: "Tears",
-        author: "Envy",
-        source: "https://soundcloud.com/nocopyrightlofi/no-copyright-chill-lofi-3",
-        cover: "https://i1.sndcdn.com/artworks-aPHISzywKs08J2iv-JKUrOg-t500x500.jpg",
-        path: path.join(__dirname, '2.mp3')
-    },
-    {
-        id: 3,
-        title: "Pearlescent",
-        author: "Matxete prod.",
-        source: "https://soundcloud.com/nocopyrightlofi/no-copyright-chill-lofi-hiphop-purlecent-by-matxete-prod",
-        cover: "https://i1.sndcdn.com/artworks-000532101198-sgbgsr-t500x500.jpg",
-        path: path.join(__dirname, '3.mp3')
-    }
-]
+  {
+    id: 1, // tidak terlalu digunakan mungkin? gunakan index ajah
+    title: "Lazy Day",
+    author: "Lofi Velvet",
+    source: "https://soundcloud.com/lofivelvet/lazy-day",
+    cover: "https://i1.sndcdn.com/artworks-0Hu58R3AY8YiIJ14-ixjYbA-t500x500.jpg",
+    path: path.join(__dirname, "1.mp3"),
+  },
+  {
+    id: 2,
+    title: "Tears",
+    author: "Envy",
+    source: "https://soundcloud.com/nocopyrightlofi/no-copyright-chill-lofi-3",
+    cover: "https://i1.sndcdn.com/artworks-aPHISzywKs08J2iv-JKUrOg-t500x500.jpg",
+    path: path.join(__dirname, "2.mp3"),
+  },
+  {
+    id: 3,
+    title: "Pearlescent",
+    author: "Matxete prod.",
+    source: "https://soundcloud.com/nocopyrightlofi/no-copyright-chill-lofi-hiphop-purlecent-by-matxete-prod",
+    cover: "https://i1.sndcdn.com/artworks-000532101198-sgbgsr-t500x500.jpg",
+    path: path.join(__dirname, "3.mp3"),
+  },
+  {
+    id: 4,
+    title: "Tokyo Cafe",
+    author: "TVARI",
+    source: "https://pixabay.com/music/beats-tvari-tokyo-cafe-159065/",
+    cover: "https://cdn.pixabay.com/audio/2023/07/22/02-53-18-138_200x200.jpg",
+    path: path.join(__dirname, "4.mp3"),
+  },
+  {
+    id: 5,
+    title: "Blue Boi",
+    author: "LAKEY INSPIRED",
+    source: "https://soundcloud.com/nocopyrightlofi/no-copyright-chill-lofi-hiphop-blue-boi-by-lakey-inspired",
+    cover: "https://i1.sndcdn.com/artworks-000531581460-2wwawa-t500x500.jpg",
+    path: path.join(__dirname, "5.mp3"),
+  },
+  {
+    id: 6,
+    title: "Can You Hear Me",
+    author: "l33",
+    source: "https://soundcloud.com/nocopyrightlofi/no-copyright-chill-lofi-hiphop-can-you-hear-me-by-l33",
+    cover: "https://i1.sndcdn.com/artworks-000554408118-v45wtd-t500x500.jpg",
+    path: path.join(__dirname, "6.mp3"),
+  },
+  {
+    id: 7,
+    title: "recover",
+    author: "eric 9odlow",
+    source: "https://soundcloud.com/nocopyrightlofi/no-copyright-chill-lofi-hiphop-recover-by-eric-9odlow",
+    cover: "https://i1.sndcdn.com/artworks-000534552807-3fgnrs-t500x500.jpg",
+    path: path.join(__dirname, "7.mp3"),
+  },
+];
