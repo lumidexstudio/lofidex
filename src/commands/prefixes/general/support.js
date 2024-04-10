90749f2f2b8af264902b6721f46ace9a48b62490
JastinXyz <jastinlinggar@gmail.com>
2024-04-10 10:49:15 +0700

    feat: vote hyperlink on support command

--- src/commands/prefixes/general/support.js
+++ src/commands/prefixes/general/support.js
@@ -9,7 +9,7 @@ module.exports = {
     async execute(message) {
         const embed = new EmbedBuilder()
             .setThumbnail(`https://cdn.discordapp.com/avatars/${message.client.config.clientID}/${message.client.user.avatar}.png`)
-            .setDescription(`Need help about the bot? or something else?\n\n${hyperlink('[Support Server]', 'https://discord.gg/b2hw59zVTx')}\n${hyperlink('[Our Website]', 'https://lumidex.id')}`)
+            .setDescription(`Need help about the bot? or something else?\n\n${hyperlink('[Support Server]', 'https://discord.gg/b2hw59zVTx')}\n${hyperlink('[Our Website]', 'https://lumidex.id')}\n${hyperlink('[Vote Me at Top.gg]', 'https://top.gg/bot/1221004354408939640?s=024e9ca934714')}`)
             .setColor('Purple');
 
         message.replyWithoutMention({ embeds: [embed] });
