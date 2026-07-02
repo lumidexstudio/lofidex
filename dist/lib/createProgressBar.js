c5242c39c848638e2973fe87cebd6b967ccfb9a7
Mayopi <cyade007@gmail.com>
2026-07-02 16:25:58 +0000

    chore: remove & ignore build dist dir

--- dist/lib/createProgressBar.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function createProgressBar(current, total) {
-    const pos = Math.ceil((current / total) * 10) || 1;
-    return `${"▬".repeat(pos - 1)}🔘${"─".repeat(10 - pos)}`;
-}
-module.exports = createProgressBar;
-//# sourceMappingURL=createProgressBar.js.map
--- dist/lib/toBoolean.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const toBoolean = (dataStr) => {
-    return !!((typeof dataStr === "string" && dataStr.toLowerCase() === "true") ||
-        dataStr === true ||
-        (typeof dataStr === "string" && Number.parseInt(dataStr, 10) === 0));
-};
-module.exports = toBoolean;
-//# sourceMappingURL=toBoolean.js.map
